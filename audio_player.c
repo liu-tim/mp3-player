2be478ad5b2420ea59361b5fa2704633bd8bb570
Tim Liu <ht2liu@uwaterloo.ca>
2017-04-02 02:34:51 -0400

    Update audio_player.c

--- audio_player.c
+++ audio_player.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -31,10 +30,24 @@ FATFS Fatfs[1];          /* File system object for each logical drive */
 uint8_t Buff[8192] __attribute__ ((aligned(4)));  /* Working buffer */
 
 FIL File1;
+FILE *display;
 DIR Dir;
 FILINFO Finfo;
 
-int PLAY = 1;
+alt_up_audio_dev * audio_dev;
+volatile long p1;
+volatile int fifospace;
+
+volatile int buffIndex = 0;
+
+
+volatile char filename[20][13];
+volatile unsigned long fileSize[20];
+volatile int fileIndex = 0;
+int cnt = 0;
+volatile uint32_t ofs;
+
+int playing = 0;
 int STOP = 0;
 
 static alt_u32 TimerFunction (void *context)
@@ -86,52 +99,139 @@ void put_rc(FRESULT rc)
     xprintf("rc=%u FR_%s\n", (uint32_t) rc, str);
 }
 
-static void button_press(void* context, alt_u32 id){
-	xprintf("play_pause \n");
-	xprintf("%d \n", IORD(BUTTON_PIO_BASE,0));
-//	switch (IORD(BUTTON_PIO_BASE,0)) {
-////		case 14:
-////			// seek / forward
-//		case 13:
-//			// play / pause
-//			if (PLAY) {
-//				PLAY = 0;
-//				IOWR(BUTTON_PIO_BASE,3,0);
-//				wait();
-//			} else {
-//				PLAY = 1;
-//				IOWR(BUTTON_PIO_BASE,3,0);
-//				main();
-//			}
-//		case 11:
-//			STOP = 1;
-//			PLAY = 0;
-//			IOWR(BUTTON_PIO_BASE,3,0);
-////		case 7:
-////			// seek / backwards
-//		default:
-//			IOWR(BUTTON_PIO_BASE,3,0);
-//	}
-	IOWR(BUTTON_PIO_BASE,3,0);
+
+int isWav(char *filename){
+	int len = strlen(filename);
+	return filename[len - 4] == '.' && filename[len - 3] == 'W' && filename[len - 2] == 'A' && filename[len - 1] == 'V' && filename[len] == '\0';
 }
 
-
-int main(void){
-	char Line[256];                 /* Console input buffer */
-	long p1;
-
-	char filename[20][13];
-	unsigned long fileSize[20];
-
-	char *ptr;
-	uint32_t ofs;
-	int fifospace;
-	alt_up_audio_dev * audio_dev;
-	int cnt=0, i;
+/*
+ stores the .wav files in the filename array, and the file sizes in the fileSize array
+ */
+void fileList() {
+	int i = 0;
+	xprintf("in filelist \n");
 	uint8_t res;
+	char *ptr;
+	while (*ptr == ' ')
+		ptr++;
+
+	res = f_opendir(&Dir, ptr);
+	if (res) {
+		put_rc(res);
+		return;
+	}
+	for (;;) {
+		res = f_readdir(&Dir, &Finfo);
+		if ((res != FR_OK) || !Finfo.fname[0]) {
+			break;
+		}
+		if (isWav(&(Finfo.fname[0]))) {
+			strcpy(&filename[i][0], &(Finfo.fname[0]));
+			fileSize[i++] = Finfo.fsize;
+		}
+	}
+}
+
+void loadFile () {
+    put_rc(f_open(&File1, filename[fileIndex], (uint8_t) 1)); // fo -- 1 == p1
+    p1 = fileSize[fileIndex];
+    return;
+}
+//
+void debounce (int button) {
+//	xprintf("debounce\n");
+	usleep(15000);
+	while(IORD(BUTTON_PIO_BASE,0) == button); // pass when we are no longer detecting the button
+	usleep(15000);
+}
+
+void stop () {
+	xprintf("stopping");
+	loadFile();
+	printDisplay();
+	return;
+}
+
+void play() {
 	unsigned int l_buf;
 	unsigned int r_buf;
+	uint8_t res;
+    ofs = File1.fptr;
+    if ((uint32_t) p1 >= 0) { // song has not ended
+    	// refilling buffer
+		if (buffIndex >= cnt && (uint32_t) p1 >= 512) {//what does buffIndex >= cnt do
+			cnt = 512;
+			p1 -= 512;
+			res = f_read(&File1, Buff, cnt, &cnt);
+			buffIndex = 0;
 
+		} else if (buffIndex >= cnt) { // reached the end of the song / file
+			xprintf("END OF SONG");
+			cnt = p1;
+			p1 = 0;
+			res = f_read(&File1, Buff, cnt, &cnt);
+			buffIndex = 0;
+			playing = 0;
+			stop();
+			return;
+		}
+		// writing
+		fifospace = alt_up_audio_write_fifo_space(audio_dev, ALT_UP_AUDIO_RIGHT);
+		if (fifospace > 0) {
+			l_buf = Buff[buffIndex] | Buff[buffIndex + 1] << 8;
+			r_buf = Buff[buffIndex + 2] | Buff[buffIndex + 3] << 8;
+			// write audio buffer
+			alt_up_audio_write_fifo (audio_dev, &(l_buf), 1, ALT_UP_AUDIO_LEFT);
+			alt_up_audio_write_fifo (audio_dev, &(r_buf), 1, ALT_UP_AUDIO_RIGHT);
+			buffIndex +=4;
+		}
+
+    }
+    return;
+//
+//	while (p1) {
+//		fifospace = alt_up_audio_write_fifo_space(audio_dev, ALT_UP_AUDIO_RIGHT);
+//		if (fifospace > 0) {
+//			l_buf = Buff[buffIndex] | Buff[buffIndex + 1] << 8;
+//			r_buf = Buff[buffIndex + 2] | Buff[buffIndex + 3] << 8;
+//			buffIndex +=4;
+//			// write audio buffer
+//			alt_up_audio_write_fifo (audio_dev, &(l_buf), 1, ALT_UP_AUDIO_LEFT);
+//			alt_up_audio_write_fifo (audio_dev, &(r_buf), 1, ALT_UP_AUDIO_RIGHT);
+//		}
+//
+//	}
+//	playing = 0;
+}
+
+void printDisplay() {
+	fprintf(display, "%c%s", 27, "[2J"); // clears the screen
+	fprintf(display, "%s\n", filename[fileIndex]);
+}
+
+void skipForward() {
+	fileIndex = (fileIndex + 1) % 13;
+	loadFile();
+	printDisplay();
+}
+
+void fastForwards() {
+	if (playing) {
+		buffIndex += 8;
+	}
+}
+
+void skipBackward() {
+	fileIndex = (fileIndex > 0) ? fileIndex - 1 : 12;
+	loadFile();
+	xprintf("%d", fileIndex);
+	printDisplay();
+}
+
+void init() {
+	// open up display
+	display = fopen("/dev/lcd_display", "w");
 	// open the Audio port
 	audio_dev = alt_up_audio_open_dev ("/dev/Audio");
 	if ( audio_dev == NULL)
@@ -139,121 +239,85 @@ int main(void){
 	else
 	alt_printf ("Opened audio device \n");
 	IoInit();
+	xprintf("rc=%d\n", (uint16_t) disk_initialize((uint8_t) 0)); // di	--  0 == p1
+	put_rc(f_mount((uint8_t) 0, &Fatfs[0])); // fi	-- 0 == p1
+	fileList(); // sets up file system
+}
 
-	//init interupt
-	IOWR(BUTTON_PIO_BASE,2,15);
-	alt_irq_register(BUTTON_PIO_IRQ, (void*)0, button_press);
-	IOWR(BUTTON_PIO_BASE,3,0);
-
-
-	// own init code
-
-    xprintf("rc=%d\n", (uint16_t) disk_initialize((uint8_t) 0)); // di	--  0 == p1
-    put_rc(f_mount((uint8_t) 0, &Fatfs[0])); // fi	-- 0 == p1
-
-
-    i = fileList(filename, fileSize);
+int main(void){
+	init();
+    fileIndex = 0;
 	xprintf("list of files \n");
 	int a;
-    for (a = 0; a < i; a++) {
-		xprintf("%s", filename[a]);
-		xprintf("\n");
-    }
-
-    int q = 3; // song index number
-
-    xprintf("hello \n");
-    ptr = filename[q];
-
-    put_rc(f_open(&File1, ptr, (uint8_t) 1)); // fo -- 1 == p1
-    p1 = fileSize[q];
-
-    ofs = File1.fptr;
-
-    while (p1) {
-    	if (p1 <= 0){
-    		break;
-    	}
-		if ((uint32_t) p1 >= 512) {
-			cnt = 512;
-			p1 -= 512;
-		} else {
-			cnt = p1;
-			p1 = 0;
-		}
-		res = f_read(&File1, Buff, cnt, &cnt);
-		if (res != FR_OK) {
-			put_rc(res);
-			break;
-		}
-		if (!cnt)
-			break;
-		ofs += 512;
-		int words = 0;
-		int i = 0;
-
-		while (i < (cnt -5)){
-			fifospace = alt_up_audio_write_fifo_space(audio_dev, ALT_UP_AUDIO_RIGHT); // try this for now
-
-			while ( i < (cnt - 5) && words < fifospace) {
-				l_buf = Buff[i] | Buff[++i] << 8;
-				r_buf = Buff[++i] | Buff[++i] << 8;
-				i++;
-				// write audio buffer
-				alt_up_audio_write_fifo (audio_dev, &(l_buf), 1, ALT_UP_AUDIO_LEFT);
-				alt_up_audio_write_fifo (audio_dev, &(r_buf), 1, ALT_UP_AUDIO_RIGHT);
-				words++;
-			}
-		}
-
-    }
-//	char filename[] = "abc.wav";
-//	xprintf("%d", isWav(filename));
-	return 0;
-}
-
-int isWav(char *filename){
-	int len = strlen(filename);
-//	char *src;
-//	strcpy(src, ".WAV");
-//	return filename[len - 4] == src;
-	return filename[len - 4] == '.' && filename[len - 3] == 'W' && filename[len - 2] == 'A' && filename[len - 1] == 'V' && filename[len] == '\0';
-}
-
-/*
- stores the .wav files in the filename array, and the file sizes in the fileSize array
- returns the number of .wav files
- */
-int fileList(char filename[][13], unsigned long *fileSize) {
-	int i = 0;
-	xprintf("in filelist \n");
+	char button;
 	uint8_t res;
-	char *ptr;
-	while (*ptr == ' ')
-		ptr++;
-	res = f_opendir(&Dir, ptr);
-	if (res) {
-		put_rc(res);
-		return 0;
-	}
-	for (;;) {
-		res = f_readdir(&Dir, &Finfo);
-		if ((res != FR_OK) || !Finfo.fname[0]) {
-			break;
-		}
-		xprintf("%d\n", i);
-		if (isWav(&(Finfo.fname[0]))) {
-			strcpy(&filename[i][0], &(Finfo.fname[0]));
-			fileSize[i++] = Finfo.fsize;
-//			xprintf("%9lu  %s", Finfo.fsize, &(Finfo.fname[0]));
-		}
-	}
-	return i;
-}
+    loadFile();
+    buffIndex = 0;
+//    play();
+	printDisplay();
 
-void wait() {
-	while(1) {
-		xprintf("waiting\n");
-	}
+    while(1) { // tight polling
+    	button = IORD(BUTTON_PIO_BASE,0);
+    	switch (button) {
+			case 7: // seek / backwards
+				skipBackward();
+    			debounce(7);
+				break;
+    		case 11: // stop
+    			playing = 0;
+    			stop();
+    			debounce(11);
+    			break;
+    		case 13: // play / pause
+    			xprintf("IM HERE\n");
+    			if (playing) {
+    				// pause code
+    				printDisplay();
+    				playing = 0;
+    				printDisplay();
+    			} else { // playing code
+    				playing = 1;
+
+    				//playing = 1;
+    				if ((uint32_t) p1 >= 512) {
+						cnt = 512;
+						p1 -= 512;
+					} else {
+						cnt = p1;
+						p1 = 0;
+					}
+    				res = f_read(&File1, Buff, cnt, &cnt);
+    				// change this to for loop?
+    				// fill buffer
+
+    				for (buffIndex = 0; buffIndex < 512;){
+        				unsigned int l_buf;
+        				unsigned int r_buf;
+    					fifospace = alt_up_audio_write_fifo_space(audio_dev, ALT_UP_AUDIO_RIGHT);
+    					if (fifospace > 0) {
+    						l_buf = Buff[buffIndex] | Buff[buffIndex + 1] << 8;
+    						r_buf = Buff[buffIndex + 2] | Buff[buffIndex + 3] << 8;
+    						// write audio buffer
+    						alt_up_audio_write_fifo (audio_dev, &(l_buf), 1, ALT_UP_AUDIO_LEFT);
+    						alt_up_audio_write_fifo (audio_dev, &(r_buf), 1, ALT_UP_AUDIO_RIGHT);
+    						buffIndex +=4;
+    					}
+    				}
+    				// add playing  = 1 here?
+    				printDisplay();
+    			}
+    			debounce(13);
+    			break;
+    		case 14: // seek / forward
+    			skipForward();
+    			debounce(14);
+    			break;
+    		default:
+    			if (playing) {
+    				play();
+    			}
+    	}
+    }
+	return 0;
 }
 
